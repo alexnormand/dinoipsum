55101101ee42c6cbf8dbd68d1d0b84ca27601a98
Alex Normand <anormnd@gmail.com>
2013-06-22 02:38:44 +0200

    added bootstrap-modal shim

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,13 @@
 require.config({
   baseUrl: './',
   paths: {
-    'jquery': '../components/jquery/jquery',
-    'bootstrap-modal': '../components/bootstrap/js/bootstrap-modal',
+    'jquery'              : '../components/jquery/jquery',
+    'bootstrap-modal'     : '../components/bootstrap/js/bootstrap-modal',
     'bootstrap-transition': '../components/bootstrap/js/bootstrap-transition'
-
   },
   shim: {
-    'bootstrap-modal': ['jquery']
+    'bootstrap-modal'     : ['jquery'],
+    'bootstrap-transition': ['jquery']
   }
 });
 
