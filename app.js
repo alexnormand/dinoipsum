82b38e03a4ca07a574234c65718f7120df690154
Alex Normand <anormnd@gmail.com>
2013-07-21 01:11:12 +0200

    allow cross domain requests (CORS

--- app.js
+++ app.js
@@ -11,10 +11,24 @@ var express   = require('express'),
     path      = require('path');
 
 var app = express();
+var allowCrossDomain = function allowCrossDomain(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
 
 app.configure(function() {
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
+  app.use(allowCrossDomain);
   app.use(express.compress());
   app.use(stylus.middleware({
     src: __dirname ,
