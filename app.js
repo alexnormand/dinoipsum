76525e24c7b6250904fcca84b18e84334dce5b6f
Alex Normand <anormnd@gmail.com>
2021-09-12 12:21:29 +0200

    use 301 redirection HTTP code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(function(req, res, next) {
-  res.redirect('https://dinoipsum.com' + req.originalUrl);
+  res.redirect(301, 'https://dinoipsum.com' + req.originalUrl);
 });
 
 app.listen(process.env.PORT || 3000);
